e39362916c1ef0eb0e9b164f4b584ef832552f6e
Wilson Pereira <wilsonfilho425@gmail.com>
2026-05-04 02:58:32 +0000

    adicionado o importRefreshtoken

--- express-01/api/models/index.js
+++ express-01/api/models/index.js
@@ -4,6 +4,7 @@ import Sequelize from "sequelize";
 import getUserModel from "./user";
 import getMessageModel from "./message";
 import getTarefaModel from "./tarefa";
+import getRefreshTokenModel from './refreshToken';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
